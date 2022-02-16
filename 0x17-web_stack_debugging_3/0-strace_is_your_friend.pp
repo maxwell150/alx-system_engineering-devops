614ebda6c212fc9662a6c7d4fbe8f51395bc1d33
max <maxwelgithumbi@gmail.com>
2022-02-16 20:09:44 +0300

    apache fix

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# finding out why Apache is returning a 500 error and fixing the issue
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
