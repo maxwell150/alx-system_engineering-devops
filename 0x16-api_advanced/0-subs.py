b05a8112a842455899ea1c70ad504674e055211a
max <maxwelgithumbi@gmail.com>
2022-02-18 00:24:20 +0300

    rediit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given
+    subreddit. If an invalid subreddit is given, the function should return 0.
+    """
+    redditApi = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'just-max'}
+    res = requests.get(redditApi, headers)
+    if res.status_code == 404 or not res:
+        return 0
+    results = res.json().get('data')
+    return results.get('subscribers')
