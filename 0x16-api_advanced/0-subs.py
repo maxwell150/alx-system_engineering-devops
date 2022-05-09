358779461da770439e748143a5524be8943f64d1
max <maxwelgithumbi@gmail.com>
2022-05-09 17:36:25 +0300

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,16 @@ def number_of_subscribers(subreddit):
     """function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given
     subreddit. If an invalid subreddit is given, the function should return 0.
     """
-    redditApi = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'just-max'}
-    res = requests.get(redditApi, headers, allow_redirects=False)
-    if res.status_code == 404:
+    url = "https://www.reddit.com/r/"
+    url = url + subreddit + "/about/.json"
+
+    headers = {
+        'User-Agent': 'just_max',
+        'From': 'max@holbertonschool.com'
+    }
+    result = requests.get(url, headers=headers)
+    if result.status_code == 200:
+        result = result.json()
+        return result['data']['subscribers']
+    else:
         return 0
-    results = res.json()['data']['subscribers']
-    return results
